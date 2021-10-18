ccaec2197158e0cc78ea61a711df66005ca45109
ShaohanJiang <jacky63921456@163.com>
2021-10-18 15:58:19 +0800

    Add files via upload

--- Metamind_data_for_shared/fig5_abefghijkl_figure_plot/fig5.m
+++ Metamind_data_for_shared/fig5_abefghijkl_figure_plot/fig5.m
@@ -4,7 +4,7 @@ all_res = {resrdm, resoc, ressc, resoa, ressa};
 taskcolor = [0 0 1; 1 0 0; 1 0 1; 0 1 0; 0 1 1];
 
 %% fig5a ctime_bin
-
+figure;
 neauc_bins = all_res{2}.ctime_bin;
 nsub = size(neauc_bins,1);
 mbins = mean(neauc_bins);
@@ -45,20 +45,21 @@ title('ctime binned by uncertainty')
 
 %% fig 5b ctime_binbyfituncer_mean
 
-figure; 
+figure;
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
 for ii = 1:length(all_res)
     thisres = all_res{ii};
     neauc_bins = thisres.ctime_binbyfituncer_mean;
     [nsub, nbin] = size(neauc_bins);
     mbins = mean(neauc_bins);
     sembins = std(neauc_bins)/sqrt(nsub);
-    e = errorbar(mbins, sembins, 'o-','LineWidth',2.5,'MarkerFaceColor','white','MarkerEdgeColor','black');
+    e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
     e.Color = taskcolor(ii,:);
     hold on
 end
 hold off
 
-set(gca,'XTick',1:nbin, 'YTick',0.5:0.1:1, 'LineWidth',1.5)
+set(gca,'XTick',1:nbin, 'YTick',0.5:0.1:1, 'LineWidth',1)
 box off
 xlim([0.5,nbin+0.5])
 ylim([0.5, 1])
@@ -112,41 +113,65 @@ ylim([0, 1])
 %% fig 5f residroibeta_binbyfituncer_mean
 
 figure; 
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
 for ii = 1:length(all_res)
     thisres = all_res{ii};
     neauc_bins = thisres.residroibeta_binbyfituncer_mean;
     [nsub, nbin] = size(neauc_bins);
     mbins = mean(neauc_bins);
     sembins = std(neauc_bins)/sqrt(nsub);
-    e = errorbar(mbins, sembins, 'o-','LineWidth',2.5,'MarkerFaceColor','white','MarkerEdgeColor','black');
+    e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
     e.Color = taskcolor(ii,:);
     hold on
 end
 hold off
 
 % set(gca,'XTick',1:4, 'LineWidth',1.5)
-set(gca,'XTick',1:nbin, 'YTick',-60:20:60,  'LineWidth',1.4)
+set(gca,'XTick',1:nbin, 'YTick',-70:35:70,  'LineWidth',1)
 box off
 xlim([0.5,nbin+0.5])
-ylim([-60, 40])
+ylim([-70, 35])
+set(gca, 'XTickLabel', {}, 'YTickLabel',{});
 
-%% 论文 fig 5g beta_roibetarm_ufituncer
+%% fig 5g beta_resid_meanroibeta_stduncer_bined
 
 for jj = 1:length(all_res)
-    thisr{jj} = all_res{jj}.beta_roibetarm_ufituncer;
+    thisr{jj} = all_res{jj}.beta_resid_meanroibeta_stduncer_bined;
+%     [~, p, ~, t] = ttest(thisr{jj})
 end
 
 matmean = cellfun(@mean, thisr);
 matsem = bsxfun(@rdivide, cellfun(@std, thisr), sqrt(cellfun(@length, thisr)));
 
-[bar_xtick, hb, he] = errorbar_groups(matmean, matsem, 'bar_width', 0.4, 'bar_interval', 0.08,...
-    'optional_bar_arguments',{'LineWidth',1.5, 'FaceColor','flat', 'CData',taskcolor},...
-    'optional_errorbar_arguments',{'LineStyle','none', 'Marker','o','MarkerFaceColor','white', 'CapSize',4.5, 'LineWidth',1.4});
+fig = figure;
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
+[bar_xtick, hb, he] = errorbar_groups(matmean, matsem, 'bar_width', 0.7, 'bar_interval', 0.08, 'FigID', fig, ...
+    'optional_bar_arguments',{'LineWidth',1, 'FaceColor','flat', 'CData',taskcolor},...
+    'optional_errorbar_arguments',{'LineStyle','none', 'Marker','o','MarkerFaceColor','white', 'MarkerSize', 3,'CapSize',3, 'LineWidth',1});
 b = get(hb);
 b.BaseLine.LineWidth = 1.2;
-set(gca, 'linewidth', 1.2, 'YTick',-0.2:0.1:0.2);
+set(gca, 'linewidth', 1, 'YTick',-0.2:0.2:0.5);
 box off
-ylim([-0.2, 0])
+ylim([0, 0.5])
+
+% fig = figure;
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
+% xticks = 0.85+(0:4).*1.2;
+% b= bar(xticks, matmean, 'BarWidth', 0.62);
+% b.FaceColor = 'flat';
+% b.CData = taskcolor;
+% 
+% hold on
+% % xlabel(rowname')
+% errorbar(xticks,matmean, matsem, 'linestyle', 'none','LineWidth',1,'Color','k', ...
+%     'CapSize',2, 'MarkerEdgeColor','black','Marker','o', 'MarkerSize',3, 'MarkerFaceColor','white'); %
+% hold off
+% 
+% set(gca,'ytick', 0:0.1:0.5);
+% ylim([0, 0.5])
+% xlim([0, 6.6])
+% box off
+% set(gca, 'XTickLabel', {}, 'YTickLabel',{});
 
 %% fig 5h uncerresid_binbyfituncer_absmean
 
@@ -171,23 +196,24 @@ ylim([0, 1])
 %% fig 5i residroibeta_binbyresiduncer_mean
 
 figure; 
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
 for ii = 1:length(all_res)
     thisres = all_res{ii};
     neauc_bins = thisres.residroibeta_binbyresiduncer_mean;
     [nsub, nbin] = size(neauc_bins);
     mbins = mean(neauc_bins);
     sembins = std(neauc_bins)/sqrt(nsub);
-    e = errorbar(mbins, sembins, 'o-','LineWidth',2.5,'MarkerFaceColor','white','MarkerEdgeColor','black');
+    e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
     e.Color = taskcolor(ii,:);
     hold on
 end
 hold off
 
 % set(gca,'XTick',1:4, 'LineWidth',1.5)
-set(gca,'XTick',1:nbin, 'YTick',-60:20:60,  'LineWidth',1.4)
+set(gca,'XTick',1:nbin, 'YTick',-50:25:50,  'LineWidth',1)
 box off
 xlim([0.5,nbin+0.5])
-ylim([-60, 40])
+ylim([-50, 25])
 
 %% 论文 fig 5j beta_roibetarm_absresiduncer
 
