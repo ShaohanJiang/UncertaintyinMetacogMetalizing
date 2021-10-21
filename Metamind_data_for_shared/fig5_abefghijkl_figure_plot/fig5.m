889723f5b01d41fb3450bcaee6db37bff241970b
ShaohanJiang <jacky63921456@163.com>
2021-10-21 13:50:14 +0800

    Add files via upload

--- Metamind_data_for_shared/fig5_abefghijkl_figure_plot/fig5.m
+++ Metamind_data_for_shared/fig5_abefghijkl_figure_plot/fig5.m
@@ -44,7 +44,46 @@ box off
 xlim([0.5,nbin+0.5])
 ylim([0.5, 1])
 
-%% fig 5e uncerresid_binbyfituncer_std
+%% PLOT binned absuncer (revise) fig 5c
+figure;
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
+for ii = 2:5
+neauc_bins = all_res{ii}.absuncer_bin;
+nsub = size(neauc_bins,1);
+mbins = mean(neauc_bins);
+sembins = std(neauc_bins)/sqrt(nsub);
+hold on;
+e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
+e.Color = taskcolor(ii,:);
+end
+
+set(gca,'XTick',1:4, 'YTick',-2:1:0, 'LineWidth',1)
+box off
+xlim([0.75,4.25])
+ylim([-2,0])
+% title('absuncer binned by uncertainty')
+
+%% PLOT binned absuncer (revise) fig 5d
+figure;
+% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
+for ii = 1:length(all_res)
+    thisres = all_res{ii};
+    neauc_bins = thisres.absuncer_binbyfituncer_mean;
+    [nsub, nbin] = size(neauc_bins);
+    mbins = mean(neauc_bins);
+    sembins = std(neauc_bins)/sqrt(nsub);
+    e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
+    e.Color = taskcolor(ii,:);
+    hold on
+end
+hold off
+
+set(gca,'XTick',1:nbin, 'YTick',-2:1:0, 'LineWidth',1)
+box off
+xlim([0.5,nbin+0.5])
+ylim([-2, -0])
+
+%% fig 5g uncerresid_binbyfituncer_std
 neauc_bins = all_res{1}.uncerresid_binbyfituncer_std;
 nsub = size(neauc_bins,1);
 mbins = mean(neauc_bins);
@@ -90,7 +129,7 @@ box off
 xlim([0.5, 8.5])
 ylim([0, 1])
 
-%% fig 5f residroibeta_binbyfituncer_mean
+%% fig 5h residroibeta_binbyfituncer_mean
 
 figure; 
 % set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
@@ -113,7 +152,7 @@ xlim([0.5,nbin+0.5])
 ylim([-70, 35])
 set(gca, 'XTickLabel', {}, 'YTickLabel',{});
 
-%% fig 5g beta_resid_meanroibeta_stduncer_bined
+%% fig 5i beta_resid_meanroibeta_stduncer_bined
 
 for jj = 1:length(all_res)
     thisr{jj} = all_res{jj}.beta_resid_meanroibeta_stduncer_bined;
@@ -153,7 +192,7 @@ ylim([0, 0.5])
 % box off
 % set(gca, 'XTickLabel', {}, 'YTickLabel',{});
 
-%% fig 5h uncerresid_binbyfituncer_absmean
+%% fig 5j uncerresid_binbyfituncer_absmean
 
 figure;
 for ii = 1:length(all_res)
@@ -173,7 +212,7 @@ box off
 xlim([0.5,nbin+0.5])
 ylim([0, 1])
 
-%% fig 5i residroibeta_binbyresiduncer_mean
+%% fig 5k residroibeta_binbyresiduncer_mean
 
 figure; 
 % set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
@@ -195,7 +234,7 @@ box off
 xlim([0.5,nbin+0.5])
 ylim([-50, 25])
 
-%% 论文 fig 5j beta_roibetarm_absresiduncer
+%% 论文 fig 5l beta_roibetarm_absresiduncer
 
 for jj = 1:length(all_res)
     thisr{jj} = all_res{jj}.beta_roibetarm_absresiduncer;
@@ -216,8 +255,8 @@ box off
 ylim([0,0.2])
 
 
-%% fig 5k & L roibeta_mean_split, residuncer_mean_split
-tasktitle = {'RDM', 'OC', 'SC', 'OA', 'SA'};
+%% fig 5m & n roibeta_mean_split, residuncer_mean_split
+tasktitle = {'CS-DU', 'AO-DU', 'PS-DU', 'AO-DI', 'PS-DI'};
 taskcolor = [0 0 1; 1 0 0; 1 0 1; 0 1 0; 0 1 1];
 
 for jj = 1:length(all_res)
@@ -242,43 +281,5 @@ for jj = 1:length(all_res)
 %     ylim([-50,30])
 end
 
-%% PLOT binned absuncer (revise)
-% all_res = {res_rdm_dacc, res_oc_dacc, res_sc_dacc, res_oa_dacc, res_sa_dacc};
-taskcolor = [0 0 1; 1 0 0; 1 0 1; 0 1 0; 0 1 1];
 
-figure;
-% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
-for ii = 2:5
-neauc_bins = all_res{ii}.absuncer_bin;
-nsub = size(neauc_bins,1);
-mbins = mean(neauc_bins);
-sembins = std(neauc_bins)/sqrt(nsub);
-hold on;
-e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
-e.Color = taskcolor(ii,:);
-end
 
-set(gca,'XTick',1:4, 'YTick',-2:1:0, 'LineWidth',1)
-box off
-xlim([0.75,4.25])
-ylim([-2,0])
-% title('absuncer binned by uncertainty')
-
-figure;
-% set(gcf,'unit','centimeters','position',[3 5 4.52 3.45])
-for ii = 1:length(all_res)
-    thisres = all_res{ii};
-    neauc_bins = thisres.absuncer_binbyfituncer_mean;
-    [nsub, nbin] = size(neauc_bins);
-    mbins = mean(neauc_bins);
-    sembins = std(neauc_bins)/sqrt(nsub);
-    e = errorbar(mbins, sembins, 'o-','LineWidth',1,'MarkerSize',3,'MarkerFaceColor','white','MarkerEdgeColor',taskcolor(ii,:),'CapSize',2);
-    e.Color = taskcolor(ii,:);
-    hold on
-end
-hold off
-
-set(gca,'XTick',1:nbin, 'YTick',-2:1:0, 'LineWidth',1)
-box off
-xlim([0.5,nbin+0.5])
-ylim([-2, -0])
